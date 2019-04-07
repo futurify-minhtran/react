83be0c89fb3d2619dffb4e32831ebf0b453f3d3d
futurify-minhtran <minhtran@futurify.vn>
2019-04-07 10:40:05 +0700

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default ({ name }) => {
+  return <h1>Hello {name}!</h1>;
+}
+
+
+
+function Hello({name}){
+  return <h1>Hello {name}!</h1>;
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tqbnze
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tqbnze)
